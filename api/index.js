828cf5af1ea364231a9aed24b9eab72839f160c5
Sam Poder <39828164+sampoder@users.noreply.github.com>
2021-04-20 23:12:17 +0800

    Update index.js

--- api/index.js
+++ api/index.js
@@ -4,27 +4,34 @@ import fetch from "node-fetch";
 const stb = require("@jorgeferrero/stream-to-buffer");
 
 export default async (req, res) => {
-  const { challenge, event } = req.body
-  if (challenge) return await res.json({ challenge })
-  if (event.channel !== 'C01744EK4BD' || event.subtype) return res.status(200).end()
+  const { challenge, event } = req.body;
+  if (challenge) return await res.json({ challenge });
+  if (event.channel !== "C01744EK4BD" || event.subtype)
+    return res.status(200).end();
 
-  console.log(event)
-  console.log(event.text, event.ts, event.user)
+  console.log(event);
+  console.log(event.text, event.ts, event.user);
 
   try {
-    getUrlFromString(event.text)
-    await react('add', event.channel, event.ts, 'beachball')
-  } catch { return }
-  const url = getUrlFromString(event.text)
-  const videoId = url.split('v=')[1]
-  console.log(url, videoId)
+    getUrlFromString(event.text);
+    await react("add", event.channel, event.ts, "beachball");
+  } catch {
+    return;
+  }
+  const url = getUrlFromString(event.text);
+  const videoId = url.split("v=")[1];
+  console.log(url, videoId);
   if (!videoId) {
-    await react('remove', event.channel, event.ts, 'beachball')
-    await react('add', event.channel, event.ts, 'x')
-    await reply(event.channel, event.ts, `What is this trickery??? That is not a YouTube link!`)
+    await react("remove", event.channel, event.ts, "beachball");
+    await react("add", event.channel, event.ts, "x");
+    await reply(
+      event.channel,
+      event.ts,
+      `What is this trickery??? That is not a YouTube link!`
+    );
   }
 
-  res.status(200).end()
+  res.status(200).end();
 
   const vid = await ytdl(url);
 
@@ -32,9 +39,9 @@ export default async (req, res) => {
   const thirty_sec_job = new Promise((resolve) => {
     setTimeout(() => resolve(ytdl.getInfo(videoId)), 30000);
   });
-  await Promise.race([youtubedl_job, thirty_sec_job]).then((value) => {
+  await Promise.race([youtubedl_job, thirty_sec_job]).then(async (value) => {
     console.log(typeof value.page);
-    if( typeof value.page == "undefined"){
+    if (typeof value.page == "undefined") {
       const title = info.videoDetails.title;
       console.log(info);
       const form = new FormData();
@@ -44,62 +51,63 @@ export default async (req, res) => {
         knownLength: vidBuffer.length,
       });
       form.append("channels", "C01744EK4BD");
-      form.append('thread_ts', event.ts)
+      form.append("thread_ts", event.ts);
       form.append(
         "initial_comment",
         `Oui ouiiii. This reminds me of a Rembrandt I once stole. This must be worth 10,000gp.`
       );
       console.log(form);
-      await fetch('https://slack.com/api/files.upload', {
-        method: 'POST',
-        body: form
-      }).then(r => r.json()).then(r => console.log(r))
-    
-      await react('remove', event.channel, event.ts, 'beachball')
-      await react('add', event.channel, event.ts, 'youtube')
-    }
-    else{
-      await react('add', event.channel, event.ts, 'x')
-      await reply(event.channel, event.ts, `Sooooooo BIG!`)
+      await fetch("https://slack.com/api/files.upload", {
+        method: "POST",
+        body: form,
+      })
+        .then((r) => r.json())
+        .then((r) => console.log(r));
+
+      await react("remove", event.channel, event.ts, "beachball");
+      await react("add", event.channel, event.ts, "youtube");
+    } else {
+      await react("add", event.channel, event.ts, "x");
+      await reply(event.channel, event.ts, `Sooooooo BIG!`);
     }
   });
-}
+};
 
 const getUrlFromString = (str) => {
-  const urlRegex = /(\b(https?|ftp|file):\/\/[-A-Z0-9+&@#\/%?=~_|!:,.;]*[-A-Z0-9+&@#\/%=~_|])/ig
-  let url = str.match(urlRegex)[0]
-  if (url.includes('|')) url = url.split('|')[0]
-  if (url.startsWith('<')) url = url.substring(1, url.length - 1)
-  return url
-}
+  const urlRegex = /(\b(https?|ftp|file):\/\/[-A-Z0-9+&@#\/%?=~_|!:,.;]*[-A-Z0-9+&@#\/%=~_|])/gi;
+  let url = str.match(urlRegex)[0];
+  if (url.includes("|")) url = url.split("|")[0];
+  if (url.startsWith("<")) url = url.substring(1, url.length - 1);
+  return url;
+};
 
 const react = async (addOrRemove, channel, ts, reaction) =>
-  await fetch('https://slack.com/api/reactions.' + addOrRemove, {
-    method: 'POST',
+  await fetch("https://slack.com/api/reactions." + addOrRemove, {
+    method: "POST",
     headers: {
-      'Content-Type': 'application/json',
-      Authorization: `Bearer ${process.env.SLACK_BOT_TOKEN}`
+      "Content-Type": "application/json",
+      Authorization: `Bearer ${process.env.SLACK_BOT_TOKEN}`,
     },
-    body: JSON.stringify({ channel: channel, name: reaction, timestamp: ts })
+    body: JSON.stringify({ channel: channel, name: reaction, timestamp: ts }),
   })
     .then((r) => r.json())
-    .catch((err) => console.error(err))
+    .catch((err) => console.error(err));
 
 const reply = async (channel, parentTs, text, unfurl) =>
-  await fetch('https://slack.com/api/chat.postMessage', {
-    method: 'POST',
+  await fetch("https://slack.com/api/chat.postMessage", {
+    method: "POST",
     headers: {
-      'Content-Type': 'application/json',
-      Authorization: `Bearer ${process.env.SLACK_BOT_TOKEN}`
+      "Content-Type": "application/json",
+      Authorization: `Bearer ${process.env.SLACK_BOT_TOKEN}`,
     },
     body: JSON.stringify({
       channel: channel,
       thread_ts: parentTs,
       text: text,
-      parse: 'mrkdwn',
+      parse: "mrkdwn",
       unfurl_links: unfurl,
-      unfurl_media: false
-    })
+      unfurl_media: false,
+    }),
   })
     .then((r) => r.json())
-    .then((json) => json.ts)
+    .then((json) => json.ts);
