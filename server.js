b59ca710ade186f8138fe10c3afbed288c7ce1cb
Max Wofford <max@maxwofford.com>
2021-04-21 15:28:04 -0400

    Update server.js

--- server.js
+++ server.js
@@ -1,6 +1,8 @@
 const express = require('express')
 const app = express()
-app.use(express.urlencoded())
+
+app.use(express.urlencoded({extended: true}))
+app.use(express.json())
 
 app.get('/', (req, res) => {
   res.json({ping: 'pong'})
